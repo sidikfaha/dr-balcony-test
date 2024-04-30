8c764d5b48fb21591a753c44cd5297aa001e8d0c
sidikfaha <joefaha1@gmail.com>
2024-04-30 14:17:09 +0000

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Design implementation test
+
+Tu run the project, run the followed commands :
+
+```bash
+# install dependencies
+yarn install
+
+# Build the project
+yarn tailwind:build
+yarn build
+
+# Launch web server
+yarn preview
+```
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    '**/*.{html,pcss,js,css}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FF783E',
+        'primagy-ghost': '#FFF2ED',
+      },
+    },
+  },
+  plugins: [],
+}
+
